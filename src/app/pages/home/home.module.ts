bfbeb9aa5a657df75fd692882859ad88f1823630
Janopuntopy <ale.gomez@duocuc.cl>
2025-10-20 22:21:27 -0300

    guardado de animaciones para que el dani no me rete

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -5,9 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Componente1Component } from 'src/app/components/componente1/componente1.component';
+import { Componente2Component } from 'src/app/components/componente2/componente2.component';
+import { Componente3Component } from 'src/app/components/componente3.component';
+
+
 
 @NgModule({
-  declarations: [HomePage],
+  declarations: [HomePage,Componente1Component, Componente2Component, Componente3Component],
   imports: [CommonModule,
             FormsModule,
             IonicModule,
